c988d95a331cccaa4b0e3398e5936ae1e00d8869
stackdeveloper38 <84258779+stackdeveloper38@users.noreply.github.com>
2021-05-22 14:17:01 +0300

    student detay ve mail bildirim butonu

--- src/features/User/Dashboard.js
+++ src/features/User/Dashboard.js
@@ -4,10 +4,20 @@ import { userSelector, fetchUserBytoken, clearState } from './UserSlice';
 import Loader from 'react-loader-spinner';
 import { useHistory } from 'react-router-dom';
 import { Table } from 'reactstrap';
+import toast from 'react-hot-toast';
 const Dashboard = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-
+  const sendActivationlink = () => {
+    // Github fetch library : https://github.com/github/fetch
+    // Call the API page
+    fetch('http://localhost:9002/sendActivationlink')
+    .then((result) => {
+      return result.json();
+    }).then((jsonResult) => {
+      toast.error(jsonResult.message);
+    })
+  }
   useEffect(() => {
     dispatch(fetchUserBytoken({ token: localStorage.getItem('token') }));
   }, []);
@@ -23,6 +33,9 @@ const Dashboard = () => {
     localStorage.removeItem('token');
     history.push('/login');
   };
+  var divStyle = {
+    marginRight: "10px"
+  };
   const data = students;
   return (
     <div className="container mx-auto mt-3">
@@ -33,6 +46,9 @@ const Dashboard = () => {
           <button onClick={onLogOut} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded float-right">
             Log Out
           </button>
+          <button className="btn btn-primary py-2 px-4 rounded float-right" style={divStyle} onClick={sendActivationlink}>
+            Send Activation Link
+          </button>
           <div className="container mx-auto">
             <h3>Student List</h3>
           </div>
@@ -41,6 +57,7 @@ const Dashboard = () => {
             <thead>
               <tr>
                 <th>#</th>
+                <th>StudentId</th>
                 <th>Name</th>
                 <th>Surname</th>
                 <th>Email</th>
@@ -54,8 +71,54 @@ const Dashboard = () => {
                 return (
                   <Fragment>
                     <tr>
+                      <td>{d.studentId}</td>
                       <td>{d.id}</td>
-                      <td>{d.name}</td>
+                      <td>
+                      
+                   
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target={"#exampleModal-"+d.id}>
+{d.name}
+</button>
+<div class="modal fade" id={"exampleModal-"+d.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">{d.name}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+         <table class="table table-striped">
+           <thead>
+             <tr>
+               <th>Gpa</th>
+               <th>Grade</th>
+               <th>hasVoted</th>
+               <th>isCandidate</th>
+               <th>primaryDepartment</th>
+               <th>secondaryDepartment</th>
+             </tr>
+           </thead>
+           <tbody>
+             <tr>
+               <td>{d.gpa}</td>
+               <td>{d.grade}</td>
+               <td>{d.hasVoted?"Yes":"No"}</td>
+               <td>{d.isCandidate?"Yes":"No"}</td>
+               <td>{d.primaryDepartment}</td>
+               <td>{d.secondaryDepartment}</td>
+             </tr>
+           </tbody>
+         </table>
+      
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+  
+      </div>
+    </div>
+  </div>
+</div>
+                     </td>
                       <td>{d.surname}</td>
                       <td>{d.mail}</td>
                       <td>{d.status}</td>
